3b70b15fd645c4403626a014b022b252487ace64
shivamkachawar <shivamkachawar@gmail.com>
2026-05-31 23:09:35 +0530

    Progressing in google auth - creating user if email does not exists

--- src/main/java/com/shivam/urlshortner/security/OAuthSuccessHandler.java
+++ src/main/java/com/shivam/urlshortner/security/OAuthSuccessHandler.java
@@ -3,6 +3,9 @@ package com.shivam.urlshortner.security;
 import com.shivam.urlshortner.repository.UserRepository;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import com.shivam.urlshortner.entity.User;
+import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class OAuthSuccessHandler
@@ -21,7 +24,7 @@ public class OAuthSuccessHandler
             jakarta.servlet.http.HttpServletRequest request,
             jakarta.servlet.http.HttpServletResponse response,
             org.springframework.security.core.Authentication authentication
-    ) {
+    ) throws java.io.IOException {
 
         var oauthUser =
                 (org.springframework.security.oauth2.core.user.OAuth2User)
@@ -30,8 +33,47 @@ public class OAuthSuccessHandler
         String email =
                 oauthUser.getAttribute("email");
 
-        System.out.println(
-                "GOOGLE EMAIL: " + email
+        String name =
+                oauthUser.getAttribute("name");
+
+        System.out.println("GOOGLE EMAIL: " + email);
+
+        com.shivam.urlshortner.entity.User user =
+                userRepository.findByEmail(email)
+                        .orElse(null);
+
+        if (user == null) {
+
+            user = new com.shivam.urlshortner.entity.User();
+
+            user.setEmail(email);
+
+            user.setUsername(
+                    email.split("@")[0]
+            );
+
+            // temporary password
+            user.setPassword(
+                    java.util.UUID.randomUUID().toString()
+            );
+
+            user.setRole("USER");
+
+            userRepository.save(user);
+
+            System.out.println(
+                    "NEW GOOGLE USER CREATED: " + email
+            );
+
+        } else {
+
+            System.out.println(
+                    "EXISTING USER FOUND: " + email
+            );
+        }
+
+        response.getWriter().write(
+                "Google Login Success: " + email
         );
     }
 }
