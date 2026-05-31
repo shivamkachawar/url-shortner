86ebd485889137f989b04d49993c83e2bb48e649
shivamkachawar <shivamkachawar@gmail.com>
2026-05-31 21:26:34 +0530

    Add email field to user

--- src/main/java/com/shivam/urlshortner/entity/User.java
+++ src/main/java/com/shivam/urlshortner/entity/User.java
@@ -13,6 +13,9 @@ public class User {
     @Column(unique = true)
     private String username;
 
+    @Column(unique = true)
+    private String email;
+
     private String password;
 
     @Column(name = "role")
@@ -20,14 +23,23 @@ public class User {
 
     public User() {}
 
-    public User(String username, String password, String role) {
+    public User(String username,String email,  String password, String role) {
         this.username = username;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUsername() {
